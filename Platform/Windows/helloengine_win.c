9feb86455a26e473c1c7ba37fab0e567d42cc58c
JiangYuan <imjy@foxmail.com>
2021-12-15 22:10:22 +0800

    1

---
+++ Platform/Windows/helloengine_win.c
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <windowsx.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WindowProc(UWND hWnd, 
+                        UINT message, 
+                        WPARAM wParam, 
+                        LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, 
+                HINSTANCE hPrevInstance,
+                LPTSTR lpCmdLine,
+                int nCmdShow)
+{
+    
+}                        
+
---
+++ build/Empty/CMakeFiles/Empty.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Empty.dir/EmptyApplication.cpp.o"
+  "CMakeFiles/Empty.dir/EmptyApplication.cpp.o.d"
+  "Empty"
+  "Empty.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Empty.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
